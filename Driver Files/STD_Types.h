cf1f6a6ea429487fd6965505f35e56e92397c892
saifraafat111 <saifraafat111@gmail.com>
2026-04-30 16:48:23 +0300

    Create STD_Types.h

---
+++ Driver Files/STD_Types.h
@@ -0,0 +1,18 @@
+#ifndef STD_TYPES_H_
+#define STD_TYPES_H_
+
+
+typedef unsigned char       u8;    
+typedef unsigned short int  u16;  
+typedef unsigned int        u32;   
+typedef unsigned long long  u64;  
+
+typedef signed char         s8;    
+typedef signed short int    s16;  
+typedef signed int          s32;  
+typedef signed long long    s64;  
+
+typedef float               f8;    
+typedef double              d64;  
+
+#endif /* STD_TYPES_H_ */
