7807e051e2c71f9c27b2493abca85185abef6546
saifraafat111 <saifraafat111@gmail.com>
2026-04-30 16:40:38 +0300

    Update ADC_program.c

--- Driver Files/ADC_program.c
+++ Driver Files/ADC_program.c
@@ -1,1 +1,123 @@
+#include "types.h"
+#include "private.h"
+#include "interface.h"
 
+
+static u8 s_last_adc_channel = 0xFFu;
+
+static u16 ADC_ReadSelectedChannelOnce(void)
+{
+    setBIT(ADCSRA, ADSC);
+
+    while (readBIT(ADCSRA, ADSC))
+    {
+
+    }
+
+  
+    u8 low  = ADCL;
+    u8 high = ADCH;
+
+    return (u16)(((u16)(high & 0x03u) << 8) | low);
+}
+
+void adc_init(voltage_ref vref, pre_Scalar Scalar)
+{
+
+    switch (vref)
+    {
+        case AREF:
+            clearBIT(ADMUX, REFS1);
+            clearBIT(ADMUX, REFS0);
+            break;
+
+        case AVCC:
+            clearBIT(ADMUX, REFS1);
+            setBIT  (ADMUX, REFS0);
+            break;
+
+        case INTERNAL:   
+            setBIT(ADMUX, REFS1);
+            setBIT(ADMUX, REFS0);
+            break;
+
+        default:
+            clearBIT(ADMUX, REFS1);
+            setBIT  (ADMUX, REFS0);   
+            break;
+    }
+
+  
+    clearBIT(ADMUX, ADLAR);
+
+
+    clearBIT(ADCSRA, ADPS0);
+    clearBIT(ADCSRA, ADPS1);
+    clearBIT(ADCSRA, ADPS2);
+
+    switch (Scalar)
+    {
+        case DIV_2:
+       
+            break;
+
+        case DIV_4:
+            setBIT(ADCSRA, ADPS1);
+            break;
+
+        case DIV_8:
+            setBIT(ADCSRA, ADPS0);
+            setBIT(ADCSRA, ADPS1);
+            break;
+
+        case DIV_16:
+            setBIT(ADCSRA, ADPS2);
+            break;
+
+        case DIV_32:
+            setBIT(ADCSRA, ADPS0);
+            setBIT(ADCSRA, ADPS2);
+            break;
+
+        case DIV_64:
+            setBIT(ADCSRA, ADPS1);
+            setBIT(ADCSRA, ADPS2);
+            break;
+
+        case DIV_128:
+        default:
+            setBIT(ADCSRA, ADPS0);
+            setBIT(ADCSRA, ADPS1);
+            setBIT(ADCSRA, ADPS2);
+            break;
+    }
+
+
+    setBIT(ADCSRA, ADEN);
+
+    s_last_adc_channel = 0xFFu;
+}
+
+u16 analogRead(u8 channel)
+{
+
+    if (readBIT(ADCSRA, ADEN) == 0U)
+    {
+        adc_init(AVCC, DIV_128);
+    }
+
+
+    channel &= 0x07u;
+
+
+    ADMUX = (u8)((ADMUX & 0xF0u) | (channel & 0x0Fu));
+
+    if (channel != s_last_adc_channel)
+    {
+    
+        (void)ADC_ReadSelectedChannelOnce();
+        s_last_adc_channel = channel;
+    }
+
+    return ADC_ReadSelectedChannelOnce();
+}
